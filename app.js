94d56edcf5b741a1b56deed82d043c271e2fefb0
tahr-tom <20882772+tahr-tom@users.noreply.github.com>
2022-01-25 04:00:24 +0800

    Up to exercise 4.18

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express()
 const cors = require('cors')
 const usersRouter = require('./controllers/users')
 const blogsRouter = require('./controllers/blogs')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 
@@ -16,6 +17,7 @@ app.use(express.json())
 
 app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.errorHandler)
 
