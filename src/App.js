730b6a335a7127f26a60f9e730ab13a7435dcc42
tahr-tom <20882772+tahr-tom@users.noreply.github.com>
2022-01-26 05:25:06 +0800

    Up to exercise 5.3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import LoginForm from './components/LoginForm'
+import NewBlogForm from './components/NewBlogForm'
 import blogService from './services/blogs'
 import loginService from './services/login'
 
@@ -10,25 +11,56 @@ const App = () => {
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
 
+  const [title, setTitle] = useState("")
+  const [author, setAuthor] = useState("")
+  const [url, setUrl] = useState("")
+
   const handleLogin = async (event) => {
     event.preventDefault()
     
     try {
       const user = await loginService.login({username, password})
+
+      window.localStorage.setItem('loggedBlogListAppUser', JSON.stringify(user))
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
-      console.log(user)
+      
     } catch (error) {
       console.log('wrong credentials')
     }
   }
 
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedBlogListAppUser', JSON.stringify(user))
+    blogService.setToken(null)
+    setUser(null)
+  }
+
+  const addBlog = async (event) => {
+    event.preventDefault()
+    const newBlog = {title, author, url}
+
+    const returnedBlog = await blogService.create(newBlog)
+    setBlogs(blogs.concat(returnedBlog))
+    setTitle('')
+    setAuthor('')
+    setUrl('')
+  }
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
     )  
+  }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogListAppUser')
+    if(loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
   }, [])
 
   return (
@@ -42,7 +74,17 @@ const App = () => {
       :  
       <div>
         <h2>blogs</h2>
-        <p>{user.name} logged in</p>
+        <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
+
+        <NewBlogForm
+          title={title}
+          author={author}
+          url={url}
+          handleTitleChange={({target}) => setTitle(target.value)}
+          handleAuthorChange={({target}) => setAuthor(target.value)}
+          handleUrlChange={({target}) => setUrl(target.value)}
+          handleSubmit={addBlog}
+        />
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
         )}
---
+++ src/components/NewBlogForm.js
@@ -0,0 +1,49 @@
+import react from "react";
+
+const NewBlogForm = ({
+    handleSubmit,
+    handleTitleChange,
+    handleAuthorChange,
+    handleUrlChange,
+    title,
+    author,
+    url
+  }) => {
+  return ( 
+    <>
+    <h2>create new</h2>
+    <form onSubmit={handleSubmit}>
+      <div>
+        title:
+        <input 
+          type="text" 
+          name="title" 
+          value={title} 
+          onChange={handleTitleChange}
+        />
+      </div>
+      <div>
+        author:
+        <input 
+          type="text" 
+          name="author" 
+          value={author} 
+          onChange={handleAuthorChange}
+        />
+      </div>
+      <div>
+        url:
+        <input 
+          type="text" 
+          name="url" 
+          value={url} 
+          onChange={handleUrlChange}
+        />
+      </div>
+      <button type="submit">create</button>
+    </form>
+    </>
+   );
+}
+ 
+export default NewBlogForm;
