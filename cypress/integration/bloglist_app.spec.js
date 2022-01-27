23c8e208849d5a6fc9ea7cf4ba09b256fc73e49e
tahr-tom <20882772+tahr-tom@users.noreply.github.com>
2022-01-28 05:58:14 +0800

    Up to exercise 5.18

---
+++ cypress/integration/bloglist_app.spec.js
@@ -0,0 +1,38 @@
+describe('Blog app', function () {
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'AAA',
+      username: 'aaa',
+      password: 'aaa'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users', user)
+    cy.visit('http://localhost:3000')
+  })
+
+  it('Login form is shown', function() {
+    cy.contains('log in to application')
+    cy.contains('username')
+    cy.contains('password')
+    cy.contains('login')
+  })
+
+  describe('Login', function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('aaa')
+      cy.get('#password').type('aaa')
+      cy.get('button:submit').click()
+
+      cy.contains('blogs')
+      cy.contains('logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('wrongwrongwrong')
+      cy.get('#password').type('wrongwrongwrong')
+      cy.get('button:submit').click()
+
+      cy.contains('wrong username or password')
+    })
+  })
+})
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -21,6 +21,7 @@ const LoginForm = ({
           username
         <input
           type="text"
+          id='username'
           value={username}
           name="Username"
           onChange={({ target }) => setUsername(target.value)}
@@ -30,6 +31,7 @@ const LoginForm = ({
           password
         <input
           type="password"
+          id='password'
           value={password}
           name="Password"
           onChange={({ target }) => setPassword(target.value)}
