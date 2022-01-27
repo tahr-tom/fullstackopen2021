f109974fb2e7acc37d22b4e5779fcebf741475b8
tahr-tom <20882772+tahr-tom@users.noreply.github.com>
2022-01-28 06:34:44 +0800

    Up to exercise 5.19

--- cypress/integration/bloglist_app.spec.js
+++ cypress/integration/bloglist_app.spec.js
@@ -33,7 +33,27 @@ describe('Blog app', function () {
       cy.get('button:submit').click()
 
       cy.get('.error').should('contain', 'wrong username or password')
-      cy.get('.error').should('have.css', 'color', 'rgb(255, 0, 0)')
+      cy.get('.error').should('have.css', 'color', 'rgb(255, 0, 0)') // red color
+    })
+  })
+
+  describe('When logged in', function() {
+    beforeEach(function() {
+      cy.request('POST', 'http://localhost:3003/api/login', { username: 'aaa', password: 'aaa'})
+        .then(({body}) => {
+          localStorage.setItem('loggedBlogListAppUser', JSON.stringify(body))
+          cy.visit('http://localhost:3000')
+        })
+    })
+
+    it('A blog can be created', function() {
+      cy.contains('create new blog').click()
+      cy.get('#title').type('Blog from cypress')
+      cy.get('#author').type('Cypress')
+      cy.get('#url').type('https://www.cypress.io/')
+      cy.get('.formDiv > * button').click()
+      cy.get('.message').should('contain', 'Blog from cypress by Cypress added')
+      cy.get('.message').should('have.css', 'color', 'rgb(0, 128, 0)') // green color
     })
   })
 })
--- src/App.js
+++ src/App.js
@@ -83,6 +83,7 @@ const App = () => {
     if(loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON)
       setUser(user)
+      blogService.setToken(user.token)
     }
   }, [])
 
