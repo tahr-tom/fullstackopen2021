711166d8b076401e7b609ddfd5eb3e53a9168ef7
tahr-tom <20882772+tahr-tom@users.noreply.github.com>
2022-01-28 04:00:44 +0800

    Up to exercise 5.14

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
 test('renders blog title and author, but not its url and likes number by default', () => {
@@ -18,10 +18,40 @@ test('renders blog title and author, but not its url and likes number by default
     user: user
   }
 
-  const component = render(<Blog blog={blog} user={user}/>)
+  const mockHandler = jest.fn()
+  const component = render(<Blog blog={blog} user={user} like={mockHandler} remove={mockHandler}/>)
 
   expect(component.container).toHaveTextContent('foo blog')
   expect(component.container).toHaveTextContent('Foo Bar')
 
   expect(component.container.querySelector('.blog-details')).toBeNull()
+})
+
+test('renders blog details when show button has been clicked', () => {
+
+  const user = {
+    username: 'aaa',
+    password: 'aaa'
+  }
+
+  const blog = {
+    title: 'foo blog',
+    author: 'Foo Bar',
+    url: 'http://foo.bar',
+    likes: 42,
+    user: user
+  }
+
+  const mockHandler = jest.fn()
+  const component = render(<Blog blog={blog} user={user} like={mockHandler} remove={mockHandler}/>)
+
+  const button = component.getByText('show')
+  fireEvent.click(button)
+
+  expect(component.container).toHaveTextContent('foo blog')
+  expect(component.container).toHaveTextContent('Foo Bar')
+
+  expect(component.container.querySelector('.blog-details')).toBeDefined()
+  expect(component.container).toHaveTextContent('http://foo.bar')
+  expect(component.container).toHaveTextContent('42')
 })
