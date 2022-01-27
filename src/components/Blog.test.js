e701e2889307425bd357ad934a48717c62b6f270
tahr-tom <20882772+tahr-tom@users.noreply.github.com>
2022-01-28 04:13:55 +0800

    Up to exercise 5.15

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -4,7 +4,6 @@ import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
 test('renders blog title and author, but not its url and likes number by default', () => {
-
   const user = {
     username: 'aaa',
     password: 'aaa'
@@ -28,7 +27,6 @@ test('renders blog title and author, but not its url and likes number by default
 })
 
 test('renders blog details when show button has been clicked', () => {
-
   const user = {
     username: 'aaa',
     password: 'aaa'
@@ -54,4 +52,32 @@ test('renders blog details when show button has been clicked', () => {
   expect(component.container.querySelector('.blog-details')).toBeDefined()
   expect(component.container).toHaveTextContent('http://foo.bar')
   expect(component.container).toHaveTextContent('42')
+})
+
+test('click the like button twice calls event handler twice', () => {
+  const user = {
+    username: 'aaa',
+    password: 'aaa'
+  }
+
+  const blog = {
+    title: 'foo blog',
+    author: 'Foo Bar',
+    url: 'http://foo.bar',
+    likes: 42,
+    user: user
+  }
+
+  const mockRemoveHandler = jest.fn()
+  const mockLikeHandler = jest.fn()
+  const component = render(<Blog blog={blog} user={user} like={mockLikeHandler} remove={mockRemoveHandler}/>)
+
+  const showButton = component.getByText('show')
+  fireEvent.click(showButton)
+
+  const likeButton = component.getByText('like')
+  fireEvent.click(likeButton)
+  fireEvent.click(likeButton)
+
+  expect(mockLikeHandler.mock.calls).toHaveLength(2)
 })
